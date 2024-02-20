149546930a8539280873041d39f5f1d3879d7254
luetm <lukas.langenegger@hexabit.ch>
2024-02-20 17:27:03 +0100

    Same tests with bunit.

---
+++ tests/Blazored.FluentValidation.UnitTests/BasicValidation/BasicForm.razor.cs
@@ -0,0 +1,16 @@
+﻿using Blazored.FluentValidation.UnitTests.Model;
+
+namespace Blazored.FluentValidation.UnitTests.BasicValidation;
+
+public partial class BasicForm
+{
+    private readonly Person _person = new();
+    private FluentValidationValidator? _fluentValidationValidator;
+    private string _result = "";
+
+    private void SubmitValidForm() =>
+        _result = "Valid";
+
+    private void SubmitInvalidForm() =>
+        _result = "Invalid";
+}
---
+++ tests/Blazored.FluentValidation.UnitTests/BasicValidation/BasicTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+
+namespace Blazored.FluentValidation.UnitTests.BasicValidation;
+
+public class BasicTests : TestContext
+{
+    [Fact]
+    public void Validate_ValidFirstAndLastname_ValidSubmit()
+    {
+        // Act
+        var cut = RenderComponent<BasicForm>();
+        cut.Find("input[name=FirstName]").Change("John");
+        cut.Find("input[name=LastName]").Change("Doe");
+        cut.Find("button").Click();
+
+        // Assert
+        cut.Find("#result").TextContent.Should().Be("Valid");
+    }
+    
+    [Fact]
+    public void Validate_InvalidFirstName_InvalidSubmit()
+    {
+        // Act
+        var cut = RenderComponent<BasicForm>();
+        cut.Find("input[name=FirstName]").Change(" ");
+        cut.Find("input[name=LastName]").Change("Doe");
+        cut.Find("button").Click();
+
+        // Assert
+        cut.Find("#result").TextContent.Should().Be("Invalid");
+    }
+}
