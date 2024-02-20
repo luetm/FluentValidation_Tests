60d7623a1f466ad66e55e0ffeac2645b3211abc6
luetm <lukas.langenegger@hexabit.ch>
2024-02-20 14:03:01 +0100

    Added test project.

---
+++ Blazored.FluentValidation.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Blazored.FluentValidation.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1() { }
+
+    private class Fixture
+    {
+        public Fixture()
+        {
+            
+        }
+    }
+}
